aadea4ea78294fb4848cd27bb0aa6a2737eecd89
Giovanni Robira <grobira@twilio.com>
2022-04-13 16:06:23 -0300

    new task channel definition for inactive chats

--- plugin-inactive-capacity/src/InactiveCapacityPlugin.js
+++ plugin-inactive-capacity/src/InactiveCapacityPlugin.js
@@ -1,9 +1,15 @@
 import React from 'react';
-import { ChatChannelHelper, StateHelper, VERSION } from '@twilio/flex-ui';
+import { TaskList, VERSION } from '@twilio/flex-ui';
 import { FlexPlugin } from '@twilio/flex-plugin';
 
 import reducers, { namespace } from './states';
-import registerEventsHandler from "./events"
+import { evaluateInactivity } from "./utils/channels"
+import { evaluateCapacity } from "./utils/capacity"
+import FlexState from './states/FlexState';
+import { Actions } from "./states/capacityState"
+import { ActiveBubble } from './component/activeBubble';
+import { ChatChannelHelper, StateHelper } from "@twilio/flex-ui";
+import { getWorkerChannelsApi, resetCapacityApi } from "./service"
 
 const PLUGIN_NAME = 'InactiveCapacityPlugin';
 
@@ -22,7 +28,11 @@ export default class InactiveCapacityPlugin extends FlexPlugin {
    * @param manager { import('@twilio/flex-ui').Manager }
    */
   async init(flex, manager) {
+
+    flex.TaskChannels.register(this.createInactiveChatDefinition(flex, manager));
+
     this.registerReducers(manager);
+
 
     registerEventsHandler(flex, manager);
 
@@ -43,4 +53,74 @@ export default class InactiveCapacityPlugin extends FlexPlugin {
 
     manager.store.addReducer(namespace, reducers);
   }
+
+  createInactiveChatDefinition(flex, manager) {
+    const channelDefinition = flex.DefaultTaskChannels.createChatTaskChannel("InactiveChat",
+      (task) => {
+        if (task.taskChannelUniqueName === "chat") {
+          const channel = StateHelper.getChatChannelStateForTask(task);
+          return channel?.source?.attributes.activated == false
+        }
+        return false
+      }, "Whatsapp", "WhatsappBold", "#afb3a5");
+
+    const { templates } = channelDefinition;
+    const inactiveChatChannelDefinition = {
+      ...channelDefinition,
+      templates: {
+        ...templates,
+        TaskListItem: {
+          ...templates?.TaskListItem,
+          secondLine: (task) => {
+            const channelState = StateHelper.getChatChannelStateForTask(task);
+            const helper = new ChatChannelHelper(channelState);
+            const currentDiff = new Date(new Date().getTime() - channelState.source.attributes.inactiveTime)
+            const hours = new Date().getHours() - new Date(channelState.source.attributes.inactiveTime).getHours()
+            const minutes = currentDiff.getMinutes();
+            const seconds = currentDiff.getSeconds();
+
+            let adicionalInfo = ""
+            if (helper.typers.length) {
+              adicionalInfo = "typing ..."
+            } else if (helper.lastMessage) {
+              adicionalInfo = `${helper.lastMessage.source.authorName ? helper.lastMessage.source.authorName : ""}: ${helper.lastMessage.source.body}`
+            }
+
+            const timeSinceInactivation = this.formatHours(hours, minutes, seconds)
+
+            const fullText = `${timeSinceInactivation} | ${adicionalInfo} `
+            return fullText
+          }
+        },
+        TaskCard: {
+          ...templates?.TaskCard,
+          secondLine: (task) => {
+            return "inativo";
+          }
+        },
+      },
+      addedComponents: [
+        {
+          target: "TaskListButtons",
+          component: <ActiveBubble
+            key="activeBubble"
+          />,
+          options: { align: "start", sortOrder: 0 }
+        }
+      ]
+    };
+
+    return inactiveChatChannelDefinition
+  }
+
+  formatHours(hours, minutes, seconds) {
+    let fullHour = "";
+
+    if (hours > 0) {
+      fullHour = `${hours}:`;
+    }
+    fullHour = `${fullHour}${minutes}:${seconds}`
+    return fullHour;
+  }
+
 }
